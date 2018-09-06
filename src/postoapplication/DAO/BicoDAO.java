e75834611f33a86ebbe3d0312565f899d4076fab
phmstulp <phmstulp@minha.fag.edu.br>
2018-09-05 21:51:46 -0300

    BicoDAO criada e comeco da tela

--- src/postoapplication/DAO/BicoDAO.java
+++ src/postoapplication/DAO/BicoDAO.java
@@ -7,10 +7,14 @@ package postoapplication.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import postoapplication.jdbc.ConnectionFactory;
 import postoapplication.model.Bico;
+import postoapplication.model.Combustivel;
+import postoapplication.model.Tanque;
 
 /**
  *
@@ -64,27 +68,154 @@ public class BicoDAO implements GenericDAO<Bico>{
 
     @Override
     public void delete(int id) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            this.connection = new ConnectionFactory().getConnection();
+            String sql = "delete from combustivel where cd_bico = " + id;
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.execute();
+            pstm.close();            
+        }catch (SQLException ex){
+            System.out.println("Erro ao Deletar Bico");
+            ex.printStackTrace();
+        }finally {
+            this.connection.close();
+        }
     }
 
     @Override
     public Bico getById(int id) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Bico bico = null;
+        try{
+            this.connection = new ConnectionFactory().getConnection();
+            String sql = "SELECT B.CD_BICO, B.DS_BICO, B.CD_TANQUE, T.DS_TANQUE, "
+                    + "T.NR_CAPACIDADE, C.CD_COMBUSTIVEL, C.DS_COMBUSTIVEL, C.TP_COMBUSTIVEL "
+                    + "FROM BICO AS B "
+                    + "INNER JOIN TANQUE AS T ON (T.CD_TANQUE = B.CD_TANQUE) "
+                    + "INNER JOIN COMBUSTIVEL AS C ON (C.CD_COMBUSTIVEL = T.CD_COMBUSTIVEL) "
+                    + "WHERE CD_BICO = 1 " + id;
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            bico = new Bico();
+            while (rs.next()) {
+                bico.setCodigo(rs.getInt("CD_BICO"));
+                bico.setDescricao(rs.getString("DS_BICO"));
+                bico.setTanque(carregaTanque(rs.getInt("CD_TANQUE"), rs.getString("DS_TANQUE"),
+                        rs.getDouble("NR_CAPACIDADE"), rs.getInt("CD_COMBUSTIVEL"),
+                        rs.getString("DS_COMBUSTIVEL"), rs.getString("TP_COMBUSTIVEL")));
+            }
+            pstm.close();
+        }catch (SQLException ex){
+            System.out.println("Erro ao Buscar Bico");
+            ex.printStackTrace();
+        }finally {
+            this.connection.close();
+        }
+        return bico;
     }
 
     @Override
     public List<Bico> getByName(String name) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Bico bico = null;
+        List<Bico> bicoList = null;
+        try{
+            this.connection = new ConnectionFactory().getConnection();
+            String sql = "SELECT B.CD_BICO, B.DS_BICO, B.CD_TANQUE, T.DS_TANQUE, "
+                    + "T.NR_CAPACIDADE, C.CD_COMBUSTIVEL, "
+                    + "C.DS_COMBUSTIVEL, C.TP_COMBUSTIVEL "
+                    + "FROM BICO AS B "
+                    + "INNER JOIN TANQUE AS T ON (T.CD_TANQUE = B.CD_TANQUE) "
+                    + "INNER JOIN COMBUSTIVEL AS C ON (C.CD_COMBUSTIVEL = T.CD_COMBUSTIVEL) "
+                    + "WHERE UPPER(DS_BICO) LIKE UPPER('%\"+name+\"%') ";
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            bicoList = new ArrayList<>();
+            while (rs.next()) {
+                bico = new Bico();
+                bico.setCodigo(rs.getInt("CD_BICO"));
+                bico.setDescricao(rs.getString("DS_BICO"));
+                bico.setTanque(carregaTanque(rs.getInt("CD_TANQUE"), rs.getString("DS_TANQUE"),
+                        rs.getDouble("NR_CAPACIDADE"), rs.getInt("CD_COMBUSTIVEL"),
+                        rs.getString("DS_COMBUSTIVEL"), rs.getString("TP_COMBUSTIVEL")));
+                bicoList.add(bico);
+            }
+            pstm.close();
+        }catch (SQLException ex){
+            System.out.println("Erro ao consultar por Nome");
+            ex.printStackTrace();
+        }finally {
+            this.connection.close();
+        }
+        return bicoList;
     }
 
     @Override
     public List<Bico> getAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Bico> bicoList = null;
+        Bico bico = null;
+        try{
+            this.connection = new ConnectionFactory().getConnection();
+            String sql = "SELECT B.CD_BICO, B.DS_BICO, B.CD_TANQUE, T.DS_TANQUE, "
+                    + "T.NR_CAPACIDADE, C.CD_COMBUSTIVEL, "
+                    + "C.DS_COMBUSTIVEL, C.TP_COMBUSTIVEL FROM BICO AS B "
+                    + "INNER JOIN TANQUE AS T ON (T.CD_TANQUE = B.CD_TANQUE) "
+                    + "INNER JOIN COMBUSTIVEL AS C ON (C.CD_COMBUSTIVEL = T.CD_COMBUSTIVEL) "
+                    + "ORDER BY CD_COMBUSTIVEL ";
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            bicoList = new ArrayList<>();
+            while (rs.next()) {
+                bico = new Bico();
+                bico.setCodigo(rs.getInt("CD_BICO"));
+                bico.setDescricao(rs.getString("DS_BICO"));
+                bico.setTanque(carregaTanque(rs.getInt("CD_TANQUE"), rs.getString("DS_TANQUE"),
+                        rs.getDouble("NR_CAPACIDADE"), rs.getInt("CD_COMBUSTIVEL"),
+                        rs.getString("DS_COMBUSTIVEL"), rs.getString("TP_COMBUSTIVEL")));
+                bicoList.add(bico);
+            }
+            pstm.close();
+        }catch (SQLException ex){
+            System.out.println("Erro ao consultar todos Bicos");
+            ex.printStackTrace();
+        }finally {
+            this.connection.close();
+        }
+        return bicoList;
     }
 
     @Override
     public int getLastId() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement pstm = null;
+        try {
+            this.connection = new ConnectionFactory().getConnection();
+            String sql = "SELECT COALESCE(MAX(CD_BICO),0)+1 AS MAIOR FROM BICO ";
+            pstm = connection.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            while (rs.next())
+                return rs.getInt("MAIOR");
+              
+        }catch (SQLException ex){
+            System.out.println("Erro ao maior ID Combustivel");
+            ex.printStackTrace();
+        }finally {
+            pstm.close();
+            this.connection.close();
+        }
+        return 1;
+    }
+
+    private Tanque carregaTanque(int tanqueCod, String tanqueDesc, 
+            double tanqueCapacidade, int combustivelCod, String combustivelDesc, 
+            String combustivelTipo) {
+        Combustivel combustivel = new Combustivel();
+        Tanque tanque = new Tanque();
+        combustivel.setCodigo(combustivelCod);
+        combustivel.setDescricao(combustivelDesc);
+        combustivel.setTipoCombustivel(combustivelTipo);
+        tanque.setCodigo(tanqueCod);
+        tanque.setDescricao(tanqueDesc);
+        tanque.setCapacidade(tanqueCapacidade);
+        tanque.setCombustivel(combustivel);
+        return tanque;
     }
     
 }
