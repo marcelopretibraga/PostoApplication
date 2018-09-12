3d04e23b12ae42a0d590dacca5c24576c740d800
joseb <joseb@DESKTOP-4RPT2FN>
2018-09-12 20:31:40 -0300

    alterando getbyname

--- src/postoapplication/DAO/ClienteDAO.java
+++ src/postoapplication/DAO/ClienteDAO.java
@@ -128,18 +128,18 @@ public class ClienteDAO implements GenericDAO<Cliente> {
         List<Cliente> clienteList = null;
         try {
             this.connection = new ConnectionFactory().getConnection();
-            String sql = "select * from cliente where upper(nm_cliente) like upper('%" + name + "%')";
+            String sql = "select * from cliente where upper(ds_cliente) like upper('%" + name + "%')";
             PreparedStatement pstm = connection.prepareStatement(sql);
             ResultSet rs = pstm.executeQuery();
             clienteList = new ArrayList<>();
             while (rs.next()) {
                 cliente = new Cliente();
                 cliente.setCodigo(rs.getInt("cd_cliente"));
-                cliente.setNome(rs.getString("nm_cliente"));
-                cliente.setCpfCnpj(rs.getInt("nr_cpfcnpj"));
-                cliente.setTelefone(rs.getString("nr_telefone"));
-                cliente.setEndereco(rs.getString("ds_endereco"));
-                cliente.setUsuario(rs.getInt("cd_usuario"));
+                cliente.setNome(rs.getString("endereco_cliente"));
+                cliente.setCpfCnpj(rs.getInt("cpfcnpj_cliente"));
+                cliente.setTelefone(rs.getString("fone_cliente"));
+                cliente.setEndereco(rs.getString("endereco_cliente"));
+                cliente.setUsuario(rs.getInt("usuario"));
                 clienteList.add(cliente);
             }
             pstm.close();
