7d87d1fe679dfa0fe668791ba035c216bcd83797
MatheusSantosScuzziatto <matheusscuzzi@gmail.com>
2018-10-18 23:56:43 -0300

    Arrumado save e delete. Criado getLastId. getAll, getByName, getById, e update não utilizados

--- src/postoapplication/DAO/NotaFiscalDAO.java
+++ src/postoapplication/DAO/NotaFiscalDAO.java
@@ -3,6 +3,7 @@ package postoapplication.DAO;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.List;
@@ -24,43 +25,45 @@ public class NotaFiscalDAO implements GenericDAO<NotaFiscal> {
             this.connection = new ConnectionFactory().getConnection();
             this.connection.setAutoCommit(false);
             StringBuilder sql = new StringBuilder();
-            sql.append("INSERT INTO NOTAFISCAL (CD_NOTAFISCAL, NR_NOTA,")
-                    .append("DS_SERIE, CD_CLIENTE, DT_EMISSAO, VL_TOTAL")
-                    .append(" VALUES(?,?,?,?,?,?)");
-            
+            sql.append("INSERT INTO NOTAFISCAL (CD_NOTAFISCAL, NUMERO_NOTAFISCAL, ")
+                    .append("SERIE_NOTAFISCAL, DTEMISSAO_NOTAFISCAL, VLTOTAL_NOTAFISCAL, ")
+                    .append("CD_CLIENTE, DT_RECORD, DT_UPDATE) VALUES (?,?,?,?,?,?,?,?)");
             PreparedStatement pstm = connection.prepareStatement(sql.toString());
             pstm.setInt(1, entity.getCodigo());
             pstm.setInt(2, entity.getNumeroNota());
             pstm.setString(3, entity.getSerie());
-            pstm.setInt(4, entity.getCliente().getCodigo());
-            pstm.setDate(5, (Date) entity.getDataEmissao());
-            pstm.setDouble(6, entity.getValorTotal());
-        
-            PreparedStatement pstmItem = connection.prepareStatement(sql.toString()); //Pstm para os Itens
-            for (ItemNF inf : entity.getItensNF()) {
-                sql.append("INSERT INTO ITEMNF (CD_ITEMNF, CD_NOTAFISCAL,")
-                        .append("NR_QUANTIDADE, VL_UNITARIO, VL_DESCONTO,")
-                        .append("VL_TOTALITEM, CD_COMBUSTIVEL)")
-                        .append(" VALUES (?,?,?,?,?,?,?)");
-                
-                pstmItem.setInt(1, inf.getCodigo());
-                pstmItem.setInt(2, inf.getNotaFiscal().getCodigo());
-                pstmItem.setDouble(3, inf.getQuantidade());
-                pstmItem.setDouble(4, inf.getValorUnitario());
-                pstmItem.setDouble(5, inf.getValorDesconto());
-                pstmItem.setDouble(6, inf.getValorTotal());
-                pstmItem.setInt(7, inf.getCombustivel().getCodigo());
-            }
-            
+            pstm.setDate(4, (Date) entity.getDataEmissao());
+            pstm.setDouble(5, entity.getValorTotal());
+            pstm.setInt(6, entity.getCliente().getCodigo());
+            pstm.setDate(7, new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
+            pstm.setDate(8, new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
             pstm.execute();
-            pstmItem.execute();
+            StringBuilder sqlItem = new StringBuilder();
+            for (ItemNF inf : entity.getItensNF()) {
+                sqlItem.append("INSERT INTO ITEMNF (CD_ITEMNF, QT_ITEMNF, VLUNITARIO_ITEMNF, ")
+                        .append("DESCONTO_ITEMNF, VLTOTAL_ITEMNF, CD_COMBUSTIVEL, CD_NOTAFISCAL, ")
+                        .append("DT_RECORD, DT_UPDATE) VALUES (?,?,?,?,?,?,?,?,?)");
+                pstm = connection.prepareStatement(sqlItem.toString());
+                pstm.setInt(1, inf.getCodigo());
+                pstm.setDouble(2, inf.getQuantidade());
+                pstm.setDouble(3, inf.getValorUnitario());
+                pstm.setDouble(4, inf.getValorDesconto());
+                pstm.setDouble(5, inf.getValorTotal());
+                pstm.setInt(6, inf.getCombustivel().getCodigo());
+                pstm.setInt(7, inf.getNotaFiscal().getCodigo());
+                pstm.setDate(8, new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
+                pstm.setDate(9, new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
+                pstm.execute();
+            }
             pstm.close();
-            pstmItem.close();
         } catch (SQLException ex) {
             this.connection.rollback();
-            System.out.println("Erro ao inserir Combustivel.");
+            System.out.println("Erro ao inserir NF-e.");
             ex.printStackTrace();
-        } finally {//Sempre executa o Finally 
+        } catch (Exception ex) {
+            System.out.println("Erro inesperado ao inserir NF-e");
+            ex.printStackTrace();
+        } finally {
             this.connection.commit();
             connection.close();
         }
@@ -68,7 +71,8 @@ public class NotaFiscalDAO implements GenericDAO<NotaFiscal> {
 
     @Override
     public void update(NotaFiscal entity) throws SQLException {
-        
+        //METODO NAO UTILIZADO
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -76,14 +80,16 @@ public class NotaFiscalDAO implements GenericDAO<NotaFiscal> {
         try {
             this.connection = new ConnectionFactory().getConnection();
             StringBuilder sql = new StringBuilder();
-            sql.append("DELETE FROM ITEMNF WHERE CD_NOTAFISCAL = " + id)
+            sql.append("DELETE FROM ITEMNF WHERE CD_NOTAFISCAL = " + id + ";")
                 .append(" DELETE FROM NOTAFISCAL WHERE CD_NOTAFISCAL = " + id);
-            
             PreparedStatement pstm = connection.prepareStatement(sql.toString());
             pstm.execute();
             pstm.close();
         } catch (SQLException ex) {
             System.out.println("Erro ao Deletar Nota Fiscal");
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println("Erro inesperado ao Deletar Nota Fiscal");
             ex.printStackTrace();
         }finally {
             this.connection.close();
@@ -92,30 +98,43 @@ public class NotaFiscalDAO implements GenericDAO<NotaFiscal> {
 
     @Override
     public NotaFiscal getById(int id) throws SQLException {
+        //METODO NAO UTILIZADO
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<NotaFiscal> getByName(String name) throws SQLException {
+        //METODO NAO UTILIZADO
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<NotaFiscal> getAll() throws SQLException {
+        //METODO NAO UTILIZADO
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public int getLastId() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public Combustivel populaCombustivel(int codigo, String descricao, String tipo, int usuario) {
-        Combustivel comb = new Combustivel();
-        comb.setCodigo(codigo);
-        comb.setDescricao(descricao);
-        comb.setTipoCombustivel(tipo);
-        comb.setUsuario(usuario);
-        return comb;
+        PreparedStatement pstm = null;
+        try {
+            this.connection = new ConnectionFactory().getConnection();
+            String sql = "SELECT COALESCE(MAX(CD_NOTAFISCAL),0)+1 AS MAIOR FROM NOTAFISCAL ";
+            pstm = connection.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            while (rs.next())
+                return rs.getInt("MAIOR");
+              
+        }catch (SQLException ex){
+            System.out.println("Erro ao recuperar maior ID Nota Fiscal");
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println("Erro inesperado ao recuperar maior ID Nota Fiscal");
+            ex.printStackTrace();
+        }finally {
+            pstm.close();
+            this.connection.close();
+        }
+        return 1;
     }
 }
